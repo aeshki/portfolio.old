2e9b34827590d09e9fc0ef6442f3c2589b0ee82e
Sir Aeshki <73862313+aeshki@users.noreply.github.com>
2023-09-20 01:05:02 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+const libImgs = {
+    "baby-mobs": 4
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const toggleMenu = () => {
+        burger.classList.toggle('toggle');
+        document.body.classList.toggle('overflow');
+        document.querySelector('.nav-links').classList.toggle('nav-active');
+    };
+    burger.addEventListener('click', () => toggleMenu());
+    document.querySelectorAll('.nav-links a').forEach(x=>x.addEventListener('click', () => toggleMenu()));
+    document.querySelector('.btn-home').addEventListener('click', () => {
+        if (burger.classList.contains('toggle')) toggleMenu();
+    });
+
+    document.querySelectorAll('.slider').forEach(slider => {
+        const project = slider.dataset.projet;
+        const size = libImgs[project];
+        let index = 0;
+        const slide = () => {
+            if (index < 0) index = size - 1;
+            slider.children.slide.src = `assets/${project}/${index % size}.png`;
+        };
+        slider.children.previous.addEventListener('click', () => (--index, slide()));
+        slider.children.next.addEventListener('click', () => (++index, slide()));
+    })
+});
